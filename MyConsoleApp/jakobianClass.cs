b4fca4cd80059884018f51c8ea14e0de13baa201
SaniolJR <mateusz.sadowski04@wp.pl>
2025-10-24 14:57:34 +0200

    elemUni = 2D i 1D dokonczenie

--- MyConsoleApp/jakobianClass.cs
+++ MyConsoleApp/jakobianClass.cs
@@ -4,24 +4,38 @@ namespace jakobianClass
 {
     class ElemUniv
     {
+        public int npc { get; private set; }
         public List<List<double>> dN_de { get; private set; }
         public List<List<double>> dN_dn { get; private set; }
 
         //UWAGA! KLASA JEST PISANA TYLKO DLA INPUTU TABLIC 2D!
-        public ElemUniv(int npc, IReadOnlyList<IReadOnlyList<(double x, double y)>> gauss_pkt)
+        //chdzi o to żę przysyłamy tablice z gaussClass tą 2D, trzebaby nadpisac konstruktor by dzialal
+
+        public ElemUniv(int npc, IReadOnlyList<double> gauss_pkt1D)
+        {
+            this.dN_de = new List<List<double>>();
+            this.dN_dn = new List<List<double>>(0); //brak dN - 1 wymiar
+            for (int i = 0; i < npc; i++)
+            {
+                dN_de.Add(new List<double> { -0.5, 0.5 });   //tutaj sa po prostu stale
+            }
+        }
+
+        public ElemUniv(int npc, IReadOnlyList<IReadOnlyList<(double x, double y)>> gauss_pkt2D)
         {
             this.dN_de = new List<List<double>>();
             this.dN_dn = new List<List<double>>();
+            this.npc = npc;
 
             //pozyskiwanie w odpowiedniej kolejnosci (lewyo->prawo, dół -> gora)
-            for (int i = gauss_pkt.Count - 1; i >= 0; i--)
+            for (int i = gauss_pkt2D.Count - 1; i >= 0; i--)
             {
-                foreach (var j in gauss_pkt[i])
+                foreach (var wezel in gauss_pkt2D[i])
                 {
                     //od razu dodajemy listy zwracane przez funkcje pomocnicze 
                     // zawierające odpowiedno po 4 elementy tak jak w pliku
-                    dN_de.Add(ksiI(j.x));
-                    dN_dn.Add(etaI(j.y));
+                    dN_de.Add(ksiI(wezel.x));
+                    dN_dn.Add(etaI(wezel.y));
                 }
             }
 
--- MyConsoleApp/obj/Debug/net8.0/MyConsoleApp.AssemblyInfo.cs
+++ MyConsoleApp/obj/Debug/net8.0/MyConsoleApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyConsoleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b6e7ec56517f4435ea1b372b17f59fcc1e55c76f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2914fb1c07cba191ddd46cc459c73b47e1190faf")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyConsoleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyConsoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
